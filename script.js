e3b44e3bafcfbbab9f38ee3415f00294df3d9244
biplob14 <biploburrahaman@gmail.com>
2020-12-08 11:37:03 +0600

    colors working

---
+++ script.js
@@ -0,0 +1,16 @@
+// colors to show randomly
+const colors = ["green", "red", "purple", "grey", "rgba(133, 122, 200, 1)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    // apply colors on body background
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
